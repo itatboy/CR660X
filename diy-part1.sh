0f5b9908397076b67ed228d7a30be7655aafe32f
麦芒 <92285852+itatboy@users.noreply.github.com>
2024-05-28 12:27:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,21 +1,3 @@
-# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
-sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
-
-# 自定义默认IP
-sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
-
-# 修改默认主题
-# 取消原主题luci-theme-bootstrap 为默认主题
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-# 删除原默认主题
-rm -rf package/lean/luci-theme-bootstrap
-# 修改 argon 为默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# 替换主题为luci-theme-argon
-rm -rf package/lean/luci-theme-argon
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-
 # 添加插件
 # 应用过滤
 echo "src-git OAF https://github.com/destan19/OpenAppFilter" >> ./feeds.conf.default
