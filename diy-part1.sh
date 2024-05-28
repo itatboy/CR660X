a23798ae1bb6ef08f43627c5d75fec5be400501c
麦芒 <92285852+itatboy@users.noreply.github.com>
2024-05-29 00:01:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,7 @@
 # 添加插件
 # 应用过滤
 echo "src-git OAF https://github.com/destan19/OpenAppFilter" >> ./feeds.conf.default
+# 域名解析
+echo "src-git ddns_go https://github.com/sirpdboy/luci-app-ddns-go" >> ./feeds.conf.default
+
 
