ce45481df353ac4bf8c98cbcbf146dc1d9c8febd
麦芒 <92285852+itatboy@users.noreply.github.com>
2024-05-29 12:36:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,17 +1,2 @@
 # 自定义默认IP
 sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
-
-# 修改默认主题
-# 取消原主题luci-theme-bootstrap 为默认主题
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-# 删除原默认主题
-rm -rf package/lean/luci-theme-bootstrap
-# 修改 argon 为默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# 替换主题为luci-theme-argon
-rm -rf package/lean/luci-theme-argon
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-
-# 新增插件
-git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
