b0e83d9ce39d257aa8e8b1958beb24ff3e08d4e2
麦芒 <92285852+itatboy@users.noreply.github.com>
2024-06-05 13:33:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,13 +2,15 @@
 sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题
-# 取消原主题luci-theme-bootstrap 为默认主题
+# 取消luci-theme-bootstrap为默认主题，并删除
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-# 删除原默认主题
 rm -rf package/lean/luci-theme-bootstrap
 # 修改 argon 为默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# 替换主题为luci-theme-argon
+# 替换主题为luci-theme-argon，choose LUCI->Application->Luci-app-argon-config
 rm -rf package/lean/luci-theme-argon
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+
+# luci-theme-argon主题管理插件，choose LUCI->Application->Luci-app-argon-config
+rm -rf luci-app-argon-config
+git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git luci-app-argon-config
