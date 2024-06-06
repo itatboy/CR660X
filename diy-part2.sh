740eb9419146e05b9e23ece3f86281e1d9fe7d00
麦芒 <92285852+itatboy@users.noreply.github.com>
2024-06-06 23:28:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,5 +15,8 @@ git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git p
 rm -rf package/lean/luci-app-argon-config
 git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
 
-# 广告屏蔽插件
+# 广告屏蔽插件 adguardhome，choose LUCI->Application->Luci-app-adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
+
+# 动态域名解析插件 DDNS-GO，choose LUCI->Application->luci-app-ddns-go
+git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go package/lean/luci-app-ddns-go
