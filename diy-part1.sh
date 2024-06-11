ec1eee1d18310abb2201557470c0d75a5f02ed0e
麦芒 <92285852+itatboy@users.noreply.github.com>
2024-06-11 16:16:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,5 +1,1 @@
 # 添加插件
-
-# passwll，config需要勾选对应选项LUCI -> Applications -> Luci-app-passwall，子选项全部勾选
-#echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-#echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
